4cc58b482d993527aada0e2d4098e48dda5c6742
AngeloMaciel <angelomacieljr@hotmail.com>
2026-05-28 20:43:10 -0300

    chore(swagger): Warning: enables Swagger UI on live environment. Enables Swagger for testing, development and cloud learning.

--- backend/src/HiveApps.Api/Program.cs
+++ backend/src/HiveApps.Api/Program.cs
@@ -5,14 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//intentionally enabled on production for testing, development and learning purposes
     app.MapOpenApi();
-}
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
